5d38976b3d42f49476b12746e5dd181422c77288
white4ocolate <morozovyuriy11@gmail.com>
2024-10-01 18:41:08 +0300

    initial

---
+++ CryptoPortfolio/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CryptoPortfolio
+//
+//  Created by white4ocolate on 01.10.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CryptoPortfolio/CryptoPortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoPortfolioApp.swift
+//  CryptoPortfolio
+//
+//  Created by white4ocolate on 01.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct CryptoPortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
